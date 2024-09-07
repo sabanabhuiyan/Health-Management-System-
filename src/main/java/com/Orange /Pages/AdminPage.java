c63271e81ecfff252a1005615a198f3cc3422952
sabanabhuiyan <sabanabhuiyan10@gmail.com>
2024-09-07 11:57:16 -0400

    Add files via upload

--- src/main/java/com/Orange /Pages/AdminPage.java
+++ src/main/java/com/Orange /Pages/AdminPage.java
@@ -1,1 +1,25 @@
+package com.Orange.Pages;
 
+import com.Orange.BasesClass.BaseClass;
+import dev.failsafe.internal.util.Assert;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class AdminPage extends BaseClass {
+    @FindBy(xpath = "//h5[@class='oxd-text oxd-text--h5 oxd-table-filter-title']")
+    WebElement systemUserText;
+    @FindBy(xpath = "//i[@class='oxd-icon bi-plus oxd-button-icon']")
+    WebElement addUserLink;
+
+    public AdminPage(){
+        PageFactory.initElements (driver,this);
+    }
+    public boolean checkSystemUserText(){
+      return  systemUserText.isDisplayed ();
+    }
+    public AddUserPage checkAddUserLink(){
+        addUserLink.click ();
+        return new AddUserPage ();
+    }
+}
