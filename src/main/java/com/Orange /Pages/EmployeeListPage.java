b199f71534b01b7e3569c53e367645ea56ef2025
sabanabhuiyan <sabanabhuiyan10@gmail.com>
2024-09-09 10:23:45 -0400

    Add files via upload

--- src/main/java/com/Orange /Pages/EmployeeListPage.java
+++ src/main/java/com/Orange /Pages/EmployeeListPage.java
@@ -1,1 +1,20 @@
+package com.Orange.Pages;
 
+import com.Orange.BasesClass.BaseClass;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class EmployeeListPage extends BaseClass {
+
+    @FindBy(xpath ="//a[normalize-space()='Add Employee']")
+    WebElement addEmpLink;
+
+    public EmployeeListPage(){
+        PageFactory.initElements (driver,this);
+    }
+    public PIMAddEmployeePage validateAddEmpLink(){
+        addEmpLink.click ();
+        return new PIMAddEmployeePage ();
+    }
+}
