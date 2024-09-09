5cba21ac7b682520086e7c6712cc473bedbed94a
sabanabhuiyan <sabanabhuiyan10@gmail.com>
2024-09-09 10:43:01 -0400

    Add files via upload

--- src/main/java/com/Orange /Pages/ResetPasswordPage.java
+++ src/main/java/com/Orange /Pages/ResetPasswordPage.java
@@ -1,1 +1,28 @@
+package com.Orange.Pages;
 
+import com.Orange.BasesClass.BaseClass;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class ResetPasswordPage extends BaseClass {
+    @FindBy(xpath = "//input[@placeholder='Username']")
+    WebElement resetUserName;
+    @FindBy(xpath = "//button[@type='button']")
+    WebElement cancelButt;
+    @FindBy(xpath = "//button[@type='submit']")
+    WebElement RePassLink;
+
+    public ResetPasswordPage(){
+        PageFactory.initElements (driver,this);
+    }
+    public LoginPage validateCancelLink(){
+        cancelButt.click ();
+        return new LoginPage ();
+    }
+    public SendResetPasswordLinkPage validateResetPasswordButtonLink(String rus){
+        resetUserName.sendKeys (rus);
+        RePassLink.click ();
+        return new SendResetPasswordLinkPage ();
+    }
+}
