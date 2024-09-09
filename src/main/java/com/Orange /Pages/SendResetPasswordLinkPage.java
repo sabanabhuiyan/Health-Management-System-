de3c688e83e894c8a8f9a3602893d643b2a9c039
sabanabhuiyan <sabanabhuiyan10@gmail.com>
2024-09-09 10:44:20 -0400

    Add files via upload

--- src/main/java/com/Orange /Pages/SendResetPasswordLinkPage.java
+++ src/main/java/com/Orange /Pages/SendResetPasswordLinkPage.java
@@ -1,1 +1,19 @@
+package com.Orange.Pages;
 
+import com.Orange.BasesClass.BaseClass;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class SendResetPasswordLinkPage extends BaseClass {
+    @FindBy(xpath = "//h6[@class='oxd-text oxd-text--h6 orangehrm-forgot-password-title']")
+    WebElement successfulText;
+
+    public SendResetPasswordLinkPage(){
+        PageFactory.initElements (driver,this);
+    }
+    public boolean validateSuccessfulText(){
+       return successfulText.isDisplayed ();
+    }
+
+}
